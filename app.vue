beb1fd95e84abd92ea5858c1fc6cafade9873e70
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-22 21:05:22 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <span class="font-mono font-bold text-lg justify-center text-blue-700"> TEST TAILWIND</span>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "@nuxtjs/apollo", "@nuxtjs/tailwindcss"],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: "https://sirefcode.hasura.app/v1/graphql",
+        httpLinkOptions: {
+          headers: {
+            "content-type": "application/json",
+            "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
+          },
+        },
+      },
+    },
+  },
+  pinia: {
+    storesDirs: ["./stores/**"],
+  },
+  runtimeConfig: {
+    public: {
+      hasuraAdminSecret: process.env.HASURA_ADMIN_SECRET,
+    },
+  },
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "~/tailwind.config.js",
+  },
+});
