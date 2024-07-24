ab1fe599ff0208efc57c9d88fdf5c17a790540b7
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-25 06:42:17 +0700

    fixing header link

--- layouts/crudLayout.vue
+++ layouts/crudLayout.vue
@@ -4,7 +4,7 @@
       <div
         class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"
       >
-        <a href="https://flowbite.com" class="flex items-center">
+        <NuxtLink href="/" class="flex items-center">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             class="mr-3 h-6 sm:h-9"
@@ -14,7 +14,7 @@
             class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
             >Flowbite</span
           >
-        </a>
+        </NuxtLink>
         <div class="flex items-center lg:order-2">
           <button
             data-collapse-toggle="mobile-menu-2"
